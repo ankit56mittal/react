a1f9d9252f3b149501c21c2f819360aa0ea55c8c
Ankit <ankit56mittal@gmail.com>
2023-01-18 21:30:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -25,44 +25,67 @@
 //export default App;
 
 
-import React from 'react';
+// import React from 'react';
 
-function GoogleCanadaHeader() {
+// function GoogleCanadaHeader() {
+//   return (
+//     <div className="header">
+//       <Logo />
+//       <SearchBox />
+//       <SearchButtons />
+//       <LanguageNote />
+//     </div>
+//   );
+// }
+
+// function Logo() {
+//   return (
+//     <img src="https://citizenlab.ca/wp-content/webpc-passthru.php?src=https://citizenlab.ca/wp-content/uploads/2017/04/Google-Canada-Logo.jpg&nocache=1" alt="Google Canada logo" className="logo" />
+//   );
+// }
+
+// function SearchBox() {
+//   return (
+//     <div className="search-box">
+//       <input type="text" placeholder="Search" className="search-input" />
+//     </div>
+//   );
+// }
+
+// function SearchButtons() {
+//   return (
+//     <div className="search-buttons">
+//       <button  className="search-button" >Search</button>
+//       <button className="lucky-button">I'm Feeling Lucky</button>
+//     </div>
+//   );
+// }
+
+// function LanguageNote() {
+//   return <div className="language-note">Language: English </div>;
+// }
+
+// export default GoogleCanadaHeader;
+import React, { useState } from 'react';
+
+function BioPage() {
+  const [bio, setBio] = useState('');
+
+  const handleChange = (e) => {
+    setBio(e.target.value);
+  }
+
+  const handleClick = () => {
+    setBio(bio.toUpperCase());
+  }
+
   return (
-    <div className="header">
-      <Logo />
-      <SearchBox />
-      <SearchButtons />
-      <LanguageNote />
+    <div>
+      <textarea onChange={handleChange} value={bio} />
+      <button onClick={handleClick}>Convert to Uppercase</button>
+      <p>{bio}</p>
     </div>
   );
 }
 
-function Logo() {
-  return (
-    <img src="https://citizenlab.ca/wp-content/webpc-passthru.php?src=https://citizenlab.ca/wp-content/uploads/2017/04/Google-Canada-Logo.jpg&nocache=1" alt="Google Canada logo" className="logo" />
-  );
-}
-
-function SearchBox() {
-  return (
-    <div className="search-box">
-      <input type="text" placeholder="Search" className="search-input" />
-    </div>
-  );
-}
-
-function SearchButtons() {
-  return (
-    <div className="search-buttons">
-      <button  className="search-button" >Search</button>
-      <button className="lucky-button">I'm Feeling Lucky</button>
-    </div>
-  );
-}
-
-function LanguageNote() {
-  return <div className="language-note">Language: English </div>;
-}
-
-export default GoogleCanadaHeader;
+export default BioPage;
